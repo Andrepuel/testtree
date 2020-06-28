ea4c72f0627d87c0284d0ba1952e9c33c0a1de30
André Puel <andrepuel@gmail.com>
2020-06-28 19:01:48 -0300

    Support for async tests

--- mod.ts
+++ mod.ts
@@ -1,47 +1,58 @@
 import {assertEquals, equal} from 'https://deno.land/std/testing/asserts.ts';
 
-type TestContext = (test: Suite) => void;
-type TestContext2 = () => /*Promise<void>|*/void;
+type AsyncSuiteBody = (test: Suite) => Promise<void>;
+type SyncSuiteBody = (test: Suite) => void;
+type SuiteBody = AsyncSuiteBody | SyncSuiteBody;
+type AsyncTestBody = () => Promise<void>;
+type SyncTestBody = () => void;
+type TestBody = AsyncTestBody | SyncTestBody;
 
 function joinName(a: string, b?: string) {
     return [a, b].filter((x) => x).join(' ');
 }
 
-export function suite(name: string, cb: TestContext): void {
+export function suite(name: string, cb: AsyncSuiteBody): Promise<void>;
+export function suite(name: string, cb: SyncSuiteBody): void;
+export function suite(name: string, cb: SuiteBody): Promise<void>|void {
     const namer = (t: Suite) => {
-        t.suite(`${name}:`, cb);
+        return t.suite(`${name}:`, cb);
     }
     const suite = new SuiteRegister('', [], namer);
-    namer(suite);
+
+    return namer(suite);
 }
 
 export interface Suite {
-    test(name: string, cb: TestContext2): void;
-    suite(name: string, cb: TestContext): void;
+    test(name: string, cb: AsyncTestBody): Promise<void>|void;
+    test(name: string, cb: SyncTestBody): void;
+    test(name: string, cb: TestBody): Promise<void>|void;
+    suite(name: string, cb: AsyncSuiteBody): Promise<void>|void;
+    suite(name: string, cb: SyncSuiteBody): void;
+    suite(name: string, cb: SuiteBody): Promise<void>|void;
     later<T>(cb: () => Promise<T>): Promise<T>|T;
 }
 
-class SuiteRegister {
+class SuiteRegister implements Suite {
     private id: number = 0;
     private eof: boolean = false;
 
     constructor(private name: string, private idPrefix: number[], private cb: (t: Suite) => void) {
     }
 
-    test(name: string, _: TestContext2) {
+    test(name: string, _: TestBody): Promise<void>|void {
         const testName = joinName(this.name, name);
         const thisId = this.idPrefix.concat([this.id++]);
         Deno.test(testName, () => {
             const runner = new SuiteRunner('', [], testName, thisId);
-            this.cb(runner);
+            return this.cb(runner);
         });
     }
 
-    suite(name: string, cb: TestContext) {
+    suite(name: string, cb: SuiteBody): Promise<void>|void {
         const thisId = this.idPrefix.concat([this.id++]);
         const testName = joinName(this.name, name);
         const suite = new SuiteRegister(testName, thisId, this.cb);
-        cb(suite);
+        return cb(suite);
     }
 
     later<T>(cb: () => Promise<T>): T {
@@ -55,20 +66,20 @@ class SuiteRunner implements Suite {
     constructor(private name: string, private idPrefix: number[], private filterName: string, private filterId: number[]) {
     }
 
-    test(name: string, cb: TestContext2) {
+    test(name: string, cb: TestBody): Promise<void>|void {
         const thisId = this.idPrefix.concat([this.id++]);
         const testName = joinName(this.name, name);
         if (equal(thisId, this.filterId)) {
             assertEquals(testName, this.filterName, `Undeterministic test suite, should run ${JSON.stringify(this.filterName)} but is running ${JSON.stringify(testName)}`);
-            cb();
+            return cb();
         }
     }
 
-    suite(name: string, cb: TestContext) {
+    suite(name: string, cb: SuiteBody): Promise<void>|void {
         const thisId = this.idPrefix.concat([this.id++]);
         const testName = joinName(this.name, name);
         if (equal(thisId, this.filterId.slice(0, thisId.length))) {
-            cb(new SuiteRunner(testName, thisId, this.filterName, this.filterId));
+            return cb(new SuiteRunner(testName, thisId, this.filterName, this.filterId));
         }
     }
 
--- mod_test.ts
+++ mod_test.ts
@@ -1,7 +1,6 @@
 import {suite} from './mod.ts';
 import {assertEquals} from 'https://deno.land/std/testing/asserts.ts';
 
-debugger;
 let globalCounter = 0;
 
 suite('basic tree concept', (t) => {
@@ -29,4 +28,25 @@ suite('basic tree concept', (t) => {
 });
 Deno.test('basic tree concept finally', () => {
     assertEquals(globalCounter, 5);
+});
+
+async function delayed(): Promise<number> {
+    await new Promise(ok => setTimeout(ok, 100));
+    return globalCounter++;
+}
+
+await suite('with async', async (t) => {
+    const x = await t.later(delayed);
+
+    await t.test('registering did not invoke delayed', async () => {
+        assertEquals(x, 5);
+    });
+
+    await t.test('asynchronous test', async () => {
+        assertEquals(x, 6);
+        assertEquals(await delayed(), 7);
+    });
+});
+Deno.test('with async finally', () => {
+    assertEquals(globalCounter, 8);
 });
